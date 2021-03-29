d5c89d82143fef687cef5c288b4c3d656b2c3ccb
emnalabiadh <emnalolita@gmail.com>
2021-03-29 12:37:48 +0200

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
-
+import "./App.css";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+//import Footer from "./components/Footer";//
 function App() {
   return (
     <div class="App">
+      <Navbar/>
       <h1>HELLO IN OUR MERN PROJECT</h1>
+      <Home />
     </div>
   );
 }
